66d40afbcb6e90555cf60d399fbcb978c4631d24
PremanshBehl <premanshbehl978@gmail.com>
2026-02-25 10:40:30 +0530

    feat: updated logo to use official 1Fi LinkedIn company logo

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -10,9 +10,9 @@ const Logo = () => {
             title="1Fi Protocol Home"
         >
             <img
-                src="/1fi-logo.svg"
+                src="https://media.licdn.com/dms/image/v2/D560BAQFtVS5YUXESOQ/company-logo_200_200/B56ZxSK1egH8AI-/0/1770905098770/1fi_logo?e=2147483647&v=beta&t=BmgbudAxwGSO-9BdWL10VM7V7kLOQe2He5DB9YA0sLw"
                 alt="1Fi Logo"
-                className="h-[40px] w-[40px] rounded-[10px] shadow-lg shadow-purple-500/20"
+                className="h-[40px] w-[40px] rounded-[10px] shadow-lg shadow-purple-500/20 object-cover"
             />
         </div>
     );
